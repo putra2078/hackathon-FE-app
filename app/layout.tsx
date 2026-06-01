d99ede1d0e3b6d1ff4fc80f5ce57bf0f8d53adec
ArrifUber <arifbudianto299@gmail.com>
2026-06-01 13:19:45 +0800

    feat: add sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/Sidebar";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +31,16 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex">
+        <Sidebar/>
+
+        <div className="flex-1 flex flex-col min-h-screen overflow-x-hidden">
+          <main className="flex-1 p-6">
+            {children}
+
+          </main>
+        </div>
+        </body>
     </html>
   );
 }
---
+++ components/Sidebar/index.tsx
@@ -0,0 +1,69 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faBoxOpen, faShop } from '@fortawesome/free-solid-svg-icons'
+
+export default function Sidebar() {
+  const pathname = usePathname();
+  const itemsMenu = [
+    { name: "Dashboard", href: "/", icon: faHouse },
+    { name: "Produk", href: "/produl", icon: faBoxOpen },
+    { name: "Penjualan", href: "/penjualan", icon: faBoxOpen },
+    { name: "Pembelian", href: "/pembelian", icon: faBoxOpen },
+    { name: "Stok", href: "/stok", icon: faBoxOpen },
+    { name: "Pelanggan", href: "/pelanggan", icon: faBoxOpen },
+    { name: "Supplier", href: "/supplier", icon: faBoxOpen },
+  ];
+  return (
+    <aside className="bg-bg-survace p-4 w-60 flex flex-col sticky top-0 left-0 h-screen shadow overflow-y-auto scrollbar-thin">
+      <div className="flex flex-col gap-12">
+        {/* Logo */}
+        <div className="flex items-center py-3">
+          <div className="w-16">
+            <img
+              src="brand-icon.svg"
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div>
+            <h1 className="font-bold text-2xl/tight">
+              Toko<span className="text-brand-primary">Kita</span>
+            </h1>
+            <p className="text-[#4e4e4e] text-base/tight">Manajemen Toko</p>
+          </div>
+        </div>
+        {/* Nav List */}
+        <nav className="flex flex-col gap-4">
+          {itemsMenu.map((item, i) => {
+            const isActive = pathname === item.href;
+            return (
+              <Link
+                key={i}
+                href={item.href}
+                className={`flex items-center px-4 py-3 rounded-xl gap-6 transition-all ${isActive ? "bg-brand-light text-brand-primary" : "hover:bg-brand-light hover:text-brand-primary"}`}
+              >
+                <span>
+                    <FontAwesomeIcon icon={item.icon} className="text-xl"/>
+                </span>{" "}
+                {item.name}
+              </Link>
+            );
+          })}
+        </nav>
+
+          {/* Bagian bawah */}
+        <div className="flex gap-2 px-4 py-6 bg-brand-light rounded-xl items-center">
+          <div>
+            <FontAwesomeIcon icon={faShop} className="text-brand-primary text-3xl"/>
+          </div>
+          <div >
+            <h3 className="font-semibold">Toko sejahtera</h3>
+            <p className="text-xs">Pemilik</p>
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
+}
