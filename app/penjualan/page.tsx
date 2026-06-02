78b81903c6f1c1144090bee1edac2c9189b7821f
iLoveOrca1 <135596149+iLoveOrca1@users.noreply.github.com>
2026-06-02 22:47:45 +0800

    add: Transaction  menubar fix: index.tsx survace typo

--- app/penjualan/page.tsx
+++ app/penjualan/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+import { useEffect, useState } from 'react'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping, faBoxOpen, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping, faBoxOpen, faUser, faMagnifyingGlass, faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import { describe } from 'node:test'
+
 
 function Banner(){
   return (
@@ -39,12 +43,11 @@ function Card() {
         return (
           <div 
             key={index} 
-            className='min-h-18 flex items-center bg-brand-light border rounded-xl '
+            className='min-h-18 flex items-center bg-bg-surface border border-gray-200 rounded-2xl'
           >
-            <div className='m-6 bg-brand-primary text-background text-3xl min-w-16 min-h-16 rounded-xl flex items-center justify-center'>
-              <FontAwesomeIcon icon={info.icon} className='relative '/>
+            <div className='m-6 bg-brand-primary text-background text-3xl min-w-16 min-h-16 rounded-2xl flex items-center justify-center'>
+              <FontAwesomeIcon icon={info.icon} className='relative'/>
             </div>
-
             <span>
               <p>{info.title}</p>
               <strong className='text-3xl'>{info.description}</strong>
@@ -56,11 +59,107 @@ function Card() {
   )
 }
 
+function Search_Item() {
+  return (
+    <div className='p-2 text-foreground bg-background border border-gray-200 rounded-lg'>
+      <span className='mr-2'>
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </span>
+      <input 
+        type="text"
+        className='outline-none'
+        placeholder='Cari transaksi...'
+      />
+    </div>
+  )
+}
+
+function Date_Picker() {
+  const [date, setDate] = useState('')
+
+  useEffect(() => {console.log(date)},[date])
+  
+  return (
+    <>
+      { /* I'll figure it out later*/ }
+      <div className='bg-background px-3 border border-gray-200 rounded-xl'>
+        <input 
+          type="date"
+          className='outline-none'
+          onChange={e => setDate(e.target.value)}
+        />
+      </div>
+    </>
+  )
+}
+
+function Select_Status() {
+  const [status, setStatus] = useState('all');
+
+  const getLabel = (val: string) => {
+    switch(val) {
+      case 'selesai': return 'Selesai';
+      case 'proses': return 'Proses';
+      case 'batal': return 'Batal';
+      default: return 'Semua';
+    }
+  };
+
+  // useEffect(() => {
+  //   console.log(getLabel(status))
+  // }, [status])
+
+  return (
+    <div className='relative bg-background flex items-center justify-center border border-gray-200 rounded-xl'>
+      <select 
+        name="select_status" 
+        id="status" 
+        defaultValue='all'
+        onChange={e => setStatus(e.target.value)}
+        className="w-full px-14 outline-none text-transparent appearance-none select-none"
+      >
+        <option value="all" className="text-black">Semua</option>
+        <option value="selesai" className="text-black">Selesai</option>
+        <option value="proses" className="text-black">Proses</option>
+        <option value="batal" className="text-black">Batal</option>
+      </select>
+
+      <div className='absolute flex justify-between px-3 inset-0 pointer-events-none'>
+        <span>Status: {getLabel(status)}</span>
+        <span><FontAwesomeIcon icon={faAngleDown} /></span>
+      </div>
+    </div>
+  )
+}
+
+function Transaction_Menu() {
+  return (
+    <div className='flex justify-between'>
+      <div className='flex gap-4 items-center'>
+        <Search_Item />
+        <Date_Picker />
+        <Select_Status />
+      </div>
+      <button className='py-3 px-6 bg-brand-primary text-background font-bold rounded-xl'>+ Tambah transaksi</button>
+    </div> 
+  )
+}
+
+
+function Transaction() {
+  return (
+  <section className='p-6 bg-bg-surface border border-gray-200 rounded-2xl'>
+    <Transaction_Menu />
+  </section>
+  )
+}
+
 export default function Penjualan() {
   return (
     <>
       <Banner/>
       <Card/>
+      <Transaction/>
     </>
   )
 }
