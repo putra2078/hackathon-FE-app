49a4022950fa75a39d355316f9d42a8eab0d5d13
ArrifUber <arifbudianto299@gmail.com>
2026-06-02 16:35:55 +0800

    add stats card in dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import Banner from "@/components/Banner";
+import StatCard from "@/components/Shared/StatCard";
+import {faBoxOpen, faCartShopping, faUser} from "@fortawesome/free-solid-svg-icons"
 
 export default function Home(){
   return (
-    <div className="w-full flex flex-col gap-12 mx-auto">
+    <div className="w-full flex flex-col gap-6 mx-auto">
       <section id="banner-welcome" className="w-full">
         <Banner/>
+      </section>
+      <section id="stat-card" className="w-full">
+        <div className="grid grid-cols-3 gap-6">
+        <StatCard title="Total Penjualan" value={"Rp12.231.231"} subText="Profit" icon={faCartShopping}/>
+        <StatCard title="Total Produk" value={"238"} subText="Stok Tersedia" icon={faBoxOpen}/>
+        <StatCard title="Total Pelanggan" value={"1.024"} subTextGrowth="8.3%" subText="dari bulan lalu" icon={faUser}/>
+        </div>
       </section>
     </div>
   )
---
+++ components/Shared/StatCard.tsx
@@ -0,0 +1,27 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faArrowUp, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+
+
+interface StatCardProps {
+    title: string
+    value: string | number
+    icon: IconProp
+    subText?: string
+    subTextGrowth?: string 
+}
+
+export default function StatCard({icon, title, value, subText, subTextGrowth}: StatCardProps) {
+    return(
+        <div className="flex items-center gap-4 bg-bg-survace rounded-xl py-6 px-6">
+            <div className="flex items-center justify-center bg-brand-primary rounded-4xl w-20 h-20 text-white text-3xl">
+                <FontAwesomeIcon icon={icon}/>
+            </div>
+            <div>
+                <h3 className="font-semibold text-lg">{title}</h3>
+                <h2 className="font-semibold text-xl">{value}</h2>
+                <p>{subTextGrowth ? <span className="text-status-success"><FontAwesomeIcon icon={faArrowUp}/>{subTextGrowth}</span> : ''} {subText}</p>
+            </div>
+        </div>
+    )
+}
