dad55084c71e27b791fcd9810a92c9fee0b8c004
iLoveOrca1 <135596149+iLoveOrca1@users.noreply.github.com>
2026-06-21 22:33:53 +0800

    add: add filtering and search

---
+++ components/Shared/DatePicker.tsx
@@ -0,0 +1,48 @@
+"use client";
+import {DateField, DateRangePicker, RangeCalendar} from "@heroui/react";
+
+export default function DatePicker() {
+    return (
+      <DateRangePicker className="w-70 border rounded-md" endName="endDate" startName="startDate" aria-label="DateRangePicker">
+        <DateField.Group fullWidth className='border rounded-md'>
+          <DateField.Input slot="start">
+            {(segment) => <DateField.Segment segment={segment} />}
+          </DateField.Input>
+          <DateRangePicker.RangeSeparator />
+          <DateField.Input slot="end">
+            {(segment) => <DateField.Segment segment={segment} />}
+          </DateField.Input>
+          <DateField.Suffix>
+            <DateRangePicker.Trigger>
+              <DateRangePicker.TriggerIndicator />
+            </DateRangePicker.Trigger>
+          </DateField.Suffix>
+        </DateField.Group>
+        <DateRangePicker.Popover>
+          <RangeCalendar aria-label="Trip dates">
+            <RangeCalendar.Header>
+              <RangeCalendar.YearPickerTrigger>
+                <RangeCalendar.YearPickerTriggerHeading />
+                <RangeCalendar.YearPickerTriggerIndicator />
+              </RangeCalendar.YearPickerTrigger>
+              <RangeCalendar.NavButton slot="previous" />
+              <RangeCalendar.NavButton slot="next" />
+            </RangeCalendar.Header>
+            <RangeCalendar.Grid>
+              <RangeCalendar.GridHeader>
+                {(day) => <RangeCalendar.HeaderCell>{day}</RangeCalendar.HeaderCell>}
+              </RangeCalendar.GridHeader>
+              <RangeCalendar.GridBody>
+                {(date) => <RangeCalendar.Cell date={date} />}
+              </RangeCalendar.GridBody>
+            </RangeCalendar.Grid>
+            <RangeCalendar.YearPickerGrid>
+              <RangeCalendar.YearPickerGridBody>
+                {({year}) => <RangeCalendar.YearPickerCell year={year} />}
+              </RangeCalendar.YearPickerGridBody>
+            </RangeCalendar.YearPickerGrid>
+          </RangeCalendar>
+        </DateRangePicker.Popover>
+      </DateRangePicker>
+  );
+}
